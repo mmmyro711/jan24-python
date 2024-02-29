411572498d9d0cbad55ae52754cb28aa2ee6ab5d
myroslav711 <ymirosha711@gmail.com>
2024-02-29 03:11:40 +0000

    classwork

---
+++ session2/classwork2.py
@@ -0,0 +1,132 @@
+#var = 15
+#print (bin(15))
+#print (bin(64))
+
+#print (bin(15 & 64))
+#print (0b0)
+
+
+
+
+#var1 = 15
+#print (bin(var1))
+#print (15 // 1) == 7
+#print (15 // 2 // 2 // 2)
+#print ((var1 >> 3))
+
+
+#var = 2
+#print(bin(2))
+#print(2 << 4)
+    #print(2 * 2 * 2 * 2 )
+
+#var = 2
+#print(7 << 3)                    << # number << (multiply by 2) n-times
+    ###(7 * 2 * 2 * 2)
+
+#-----------------------------------------------------------------#
+#inp = int(input("Give yout number: "))
+#var = 10
+
+
+#if inp < var:
+        #print ("Your number is greater then 10")  #space = tab - indentation
+
+
+#else:
+       # print ("Your number is less than 10")
+
+#inp = input("Enter any number to test it is odd or even: ")
+
+#if (inp % 2) == 0:
+
+     #print ("The number is even")
+
+#else:
+
+     #print ("The provided number is odd")
+
+
+ ####################TASK1
+
+#num = int(input("Enter a number: "))
+#if (num % 2) == 0:
+#   print("{0} is even".format(num))
+#else:
+#   print("{0} is odd".format(num))
+
+#-------------------------------------------------------------#
+
+#age = int(input("What is your age? :"))
+
+    #if age < 15:
+     #   print("your are a kid")
+    #elif age < 20:
+     #   print("Your are a teenager")
+    #elif age < 100:
+   #     print("Your are an adult")
+  #  else:
+ #       print("Please provide correct age")
+
+#-------------------------------------------------------------------
+
+#age = int(input("What is your age? :"))
+
+#if age > 0 and age <= 15:
+#    print("your are a kid")
+#elif age > 15 and age <= 20:
+#    print("Your are a teenager")
+#elif age > 20 and age <= 100:
+ #       print("Your are an adult")
+#else:
+#        print("Please provide correct age")
+
+#-------------------------------------------------------------------
+
+#inp = input("What is answer? ")
+#if inp == 'n' or inp == 'N' or inp == 'Nah' or inp == 'No':  #etc
+#    print ("Your answered No")
+#elif inp == 'y' or inp == 'Y' or inp == 'Yeah' or inp == 'Yeap' : #etc
+#    print ("Your answered yes")
+
+#______________________________________________________________________
+
+#inp = input("What is your name? ")
+#if inp != 'myro'or inp == 'Myro':
+ #   print("Hi, boss")
+#else:
+  #  print ("Different user")
+#------------------------------------------------------------------------
+
+# for num in range(15):
+#    print(num)
+
+#------------------------------------------------------------------------
+
+#for letter in "Hello":
+#    print(letter)
+
+
+#word = input("Please enter your word: ")
+#for letter in word:
+#    print(letter)
+#
+#------------------------------------------------------------------------
+
+################ TASK2
+
+#        HOMEWORK      num=int(input("Please enter your number:"))
+
+#var = 0
+#while var != 10:
+ #   print(var)
+
+ #   var = var + 2
+    
+    
+#print ("This is the end", var)
+
+#--------------------------------------------------------------------------
+
+
+
