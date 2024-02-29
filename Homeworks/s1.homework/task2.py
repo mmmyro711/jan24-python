867f72636f495f2afcc08018f5cf257c20d3d5f5
myroslav711 <ymirosha711@gmail.com>
2024-02-29 03:15:39 +0000

    adding homework

---
+++ Homeworks/s1.homework/task2.py
@@ -0,0 +1,10 @@
+##Write a program that takes a string input from the user and reverses it.
+
+#def reverse (string) :
+ #   string = string [::-1]
+#    return string 
+#s = "Hello World!"
+#print ("The original string is : " '')
+#print (s)
+#print ("The reversed string  is :" '')
+#print (reverse(s))
