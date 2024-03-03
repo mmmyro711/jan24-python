7ba46a4c565017bb5821b8640a04029198c5fb64
Myroslav Yaremenko <160665603+mmmyro711@users.noreply.github.com>
2024-03-03 13:40:16 -0600

    Delete Homeworks directory

--- Homeworks/s1.homework/task1.py
+++
@@ -1,8 +0,0 @@
-####Write a program that takes a temperature in Celsius and converts it to Fahrenheit.
-
-#Fahrenheit = (Celsius * 1.8) + 32
-
-c = int(input("Temperature in celsium :")) 
-f = (c * 1.8) + 32
-
-print ("Temperature in fahrenheit :" , f )
